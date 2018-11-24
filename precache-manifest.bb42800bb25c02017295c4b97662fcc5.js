0840dd3f86dd795208c7db9b3fef4571b6611f85
sinataghavi <sina.tm30@gmail.com>
2018-11-25 00:18:12 +0330

    Updates

---
+++ precache-manifest.bb42800bb25c02017295c4b97662fcc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f0f67a8de50454842d1",
+    "url": "/maktabkhone-resume/static/js/runtime~main.2f0f67a8.js"
+  },
+  {
+    "revision": "98225526dd4f0c64d54d",
+    "url": "/maktabkhone-resume/static/js/main.98225526.chunk.js"
+  },
+  {
+    "revision": "3ff44abbff52d37cb05e",
+    "url": "/maktabkhone-resume/static/js/1.3ff44abb.chunk.js"
+  },
+  {
+    "revision": "98225526dd4f0c64d54d",
+    "url": "/maktabkhone-resume/static/css/main.c73000d1.chunk.css"
+  },
+  {
+    "revision": "bac02e74cf8316bb700b650ba8b9573a",
+    "url": "/maktabkhone-resume/index.html"
+  }
+];
